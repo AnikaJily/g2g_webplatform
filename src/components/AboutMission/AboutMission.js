e7f7989bf1dce80037e895cb32a0cb5fa2eb0b10
vankrav <vankrav@192.168.1.6>
2025-10-27 01:30:39 +0300

    fix mission

--- src/components/AboutMission/AboutMission.js
+++ src/components/AboutMission/AboutMission.js
@@ -8,26 +8,41 @@ export default function AboutMission() {
           <h2 className={styles.title}>Наша миссия</h2>
           
           <div className={styles.missionBox}>
-            <div className={styles.backgroundImage}>
+            
+            
+            
+            <div className={styles.speechBubbleWrapper}>
+              <div className={styles.speechBubble}>
+                <div className={styles.textContent}>
+                  <p>
+                    Мы верим, что каждая женщина заслуживает права на поддержку, безопасность и восстановление.
+                  </p>
+                  
+                  <p>
+                    Наша миссия — сделать психологическую помощь доступной для пострадавших от домашнего насилия женщин, дать им пространство для исцеления и вернуть веру в себя и в жизнь без страха.
+                  </p>
+                  
+                  <p>
+                    Мы стремимся не только поддерживать женщин на пути к восстановлению, но и освещать проблему насилия, разрушать молчание вокруг неё и вдохновлять на перемены.
+                  </p>
+                </div>
+              </div>
               <img 
-                src="https://www.figma.com/api/mcp/asset/4a43dd76-ce3b-4f86-82fb-940933d597f8"
+                src="/assets/icons/tail.svg"
                 alt=""
-                className={styles.decorImage}
+                className={styles.speechBubbleTail}
               />
             </div>
             
-            <div className={styles.textContent}>
-              <p>
-                Мы верим, что каждая женщина заслуживает права на поддержку, безопасность и восстановление.
-              </p>
-              
-              <p>
-                Наша миссия — сделать психологическую помощь доступной для пострадавших от домашнего насилия женщин, дать им пространство для исцеления и вернуть веру в себя и в жизнь без страха.
-              </p>
-              
-              <p>
-                Мы стремимся не только поддерживать женщин на пути к восстановлению, но и освещать проблему насилия, разрушать молчание вокруг неё и вдохновлять на перемены.
-              </p>
+            <div className={styles.bottomRow}>
+              <div className={styles.iconContainer}>
+                <img 
+                  src="/assets/icons/Icon_smile.svg" 
+                  alt="g2g icon" 
+                  width="66" 
+                  height="58"
+                />
+              </div>
             </div>
           </div>
         </div>
